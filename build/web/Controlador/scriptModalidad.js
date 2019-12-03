fcd93e798ef0d242b8255b3089823b63a3202991
infoblazer <38802117+infoblazer@users.noreply.github.com>
2019-12-02 22:05:22 -0300

    02/12/2019
    botones cambian de color, 2 versiones de tablas, una en comentario

--- build/web/Controlador/scriptModalidad.js
+++ build/web/Controlador/scriptModalidad.js
@@ -1,3 +1,12 @@
-function Cambio(){
-    
+var count=1;
+function setColor(btn){
+    var property = document.getElementById(btn);
+    if (count==1){
+        property.style.backgroundColor = "#fe001a";
+        count=0;
+    }
+    else if (count==0){
+        property.style.backgroundColor = "#ffffff";
+        count=1;
+    }
 }
