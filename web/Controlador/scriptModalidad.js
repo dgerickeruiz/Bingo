375e32eab3083d018e9059623f3fbd3fa9b55047
Alfonsonrx <damian.contreras036@gmail.com>
2020-09-24 11:58:29 -0300

    Cambie el if a switch de scriptModalidad, mas responsividad en los css de cada html ademas de centrar el contenido de bingo.html

--- web/Controlador/scriptModalidad.js
+++ web/Controlador/scriptModalidad.js
@@ -37,76 +37,77 @@ function setModalidad(btn){
 **/
 var contador = 0;
 function cambiarModo(){
-    if(contador == 0){       
-        document.getElementById("modo").src="../VISTA/images/imageLineaB2.jpg";
-        contador++;
-    }
-    else if(contador == 1){
-        document.getElementById("modo").src="../VISTA/images/imageLineaI2.jpg";
-        contador++;
-    }
-    else if(contador == 2){
-        document.getElementById("modo").src="../VISTA/images/imageLineaN2.jpg";
-        contador++;
-    }
-    else if(contador == 3){
-        document.getElementById("modo").src="../VISTA/images/imageLineaG2.jpg";
-        contador++;
-    }
-    else if(contador == 4){
-        document.getElementById("modo").src="../VISTA/images/imageLineaO2.jpg";
-        contador++;
-    }
-    else if(contador == 5){
-        document.getElementById("modo").src="../VISTA/images/imageCartonC2.jpg";
-        contador++;
-    }
-    else if(contador == 6){
-        document.getElementById("modo").src="../VISTA/images/imageCartonO2.jpg";
-        contador++;
-    }
-    else if(contador == 7){
-        document.getElementById("modo").src="../VISTA/images/imageCartonL2.jpg";
-        contador++;
-    }
-    else if(contador == 8){
-        document.getElementById("modo").src="../VISTA/images/imageCartonX2.jpg";
-        contador++;
-    }
-    else if(contador == 9){
-        document.getElementById("modo").src="../VISTA/images/imageCartonN2.jpg";
-        contador++;
-    }
-    else if(contador == 10){
-        document.getElementById("modo").src="../VISTA/images/imageCartonT2.jpg";
-        contador++;
-    }
-    else if(contador == 11){
-        document.getElementById("modo").src="../VISTA/images/imageCartonU2.jpg";
-        contador++;
-    }
-    else if(contador == 12){
-        document.getElementById("modo").src="../VISTA/images/imageCartonB2.jpg";
-        contador++;
-    }
-    else if(contador == 13){
-        document.getElementById("modo").src="../VISTA/images/imageCartonI2.jpg";
-        contador++;
-    }
-    else if(contador == 14){
-        document.getElementById("modo").src="../VISTA/images/imageCartonCruzado1-2.jpg";
-        contador++;
-    }
-    else if(contador == 15){
-        document.getElementById("modo").src="../VISTA/images/imageCartonCruzado2-2.jpg";
-        contador++;
-    }
-    else if(contador == 16){
-        document.getElementById("modo").src="../VISTA/images/imageCartonCompleto2.jpg";
-        contador++;
-    }
-    else if(contador == 17){
-        contador=0;
-        cambiarModo();
+    switch(contador) {
+        case 0:
+            document.getElementById("modo").src="../VISTA/images/imageLineaB2.jpg";
+            contador++;
+            break;
+        case 1:
+            document.getElementById("modo").src="../VISTA/images/imageLineaI2.jpg";
+            contador++;
+            break;
+        case 2:
+            document.getElementById("modo").src="../VISTA/images/imageLineaN2.jpg";
+            contador++;
+            break;
+        case 3:
+            document.getElementById("modo").src="../VISTA/images/imageLineaG2.jpg";
+            contador++;
+            break;
+        case 4:
+            document.getElementById("modo").src="../VISTA/images/imageLineaO2.jpg";
+            contador++;
+            break;
+        case 5:
+            document.getElementById("modo").src="../VISTA/images/imageCartonC2.jpg";
+            contador++;
+            break;
+        case 6:
+            document.getElementById("modo").src="../VISTA/images/imageCartonO2.jpg";
+            contador++;
+            break;
+        case 7:
+            document.getElementById("modo").src="../VISTA/images/imageCartonL2.jpg";
+            contador++;
+            break;
+        case 8:
+            document.getElementById("modo").src="../VISTA/images/imageCartonX2.jpg";
+            contador++;
+            break;
+        case 9:
+            document.getElementById("modo").src="../VISTA/images/imageCartonN2.jpg";
+            contador++;
+            break;
+        case 10:
+            document.getElementById("modo").src="../VISTA/images/imageCartonT2.jpg";
+            contador++;
+            break;
+        case 11:
+            document.getElementById("modo").src="../VISTA/images/imageCartonU2.jpg";
+            contador++;
+            break;
+        case 12:
+            document.getElementById("modo").src="../VISTA/images/imageCartonB2.jpg";
+            contador++;
+            break;
+        case 13:
+            document.getElementById("modo").src="../VISTA/images/imageCartonI2.jpg";
+            contador++;
+            break;
+        case 14:
+            document.getElementById("modo").src="../VISTA/images/imageCartonCruzado1-2.jpg";
+            contador++;
+            break;
+        case 15:
+            document.getElementById("modo").src="../VISTA/images/imageCartonCruzado2-2.jpg";
+            contador++;
+            break;
+        case 16:
+            document.getElementById("modo").src="../VISTA/images/imageCartonCompleto2.jpg";
+            contador++;
+            break;
+        default:
+            contador=0;
+            cambiarModo();
     }
 }
